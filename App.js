16e49bb10a31798b779a9706ab4d088799fc1fd1
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2022-07-31 16:23:38 +0900

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as Location from 'expo-location';
+import { key } from './Api_key.js';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const API_key = key;
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg("Permission denied");
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${loc.coords.latitude}&lon=${loc.coords.longitude}&appid=${API_key}`, {
+        method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setLocation(json);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+      // setLocation(loc);
+    })();
+  }, []);
+
+  if (errorMsg !== null) {
+    // there's been an error
+    return (
+      <View style={styles.container}>
+        <Text>There's been an error: {errorMsg}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else if (location !== null) {
+    // success
+    return (
+      <View style={styles.container}>
+        {/* <Text>{JSON.stringify(location)}</Text> */}
+        <Text>{location.name}</Text>
+        <Image
+          source={{
+            uri: `https://openweathermap.org/img/wn/${location.weather[0].icon}@2x.png`
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    // waiting
+    return (
+      <View style={styles.container}>
+        <Text>Waiting...</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
